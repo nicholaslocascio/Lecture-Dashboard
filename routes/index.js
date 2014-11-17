9b61197c3db20a92a7e7d67279560c9d1edfe3c0
Nick Locascio <nicholasjlocascio@gmail.com>
2014-11-16 21:29:11 -0500

    change file names

--- routes/index.js
+++ routes/index.js
@@ -7,37 +7,37 @@ var model = require('../data/model');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 router.get(new RegExp('session\/(.+)'), function(req, res) {
-	var session = req.params[0];
-	    model.Lecture.findOne({
-	      _id: session
-	    }, function(err, lecture) {
-	      if (lecture) {
-	        res.render('lecture');
-	      } else {
-	        return res.send(404);
-	      }
-	    });
+  var session = req.params[0];
+  model.Lecture.findOne({
+    _id: session
+  }, function(err, lecture) {
+    if (lecture) {
+      res.render('session');
+    } else {
+      return res.send(404);
+    }
+  });
 });
 
 router.get(new RegExp('class\/(.+)'), function(req, res) {
-	var slug = req.params[0];
-	    model.Lecture.findOne({
-	      slug: slug
-	    }, function(err, lecture) {
-	      if (lecture) {
-	        res.render('student');
-	      } else {
-	        return res.send(404);
-	      }
-	    });
+  var slug = req.params[0];
+  model.Lecture.findOne({
+    slug: slug
+  }, function(err, lecture) {
+    if (lecture) {
+      res.render('student');
+    } else {
+      return res.send(404);
+    }
+  });
 });
 
 router.get('/test', function(req, res) {
-   res.render('test');
+  res.render('test');
 });
 
 module.exports = router;
