1b5ea329d876d3bad51fd3fefaa6c3ec29d26cec
Matt Susskind <msuss@mit.edu>
2014-11-16 22:14:47 -0500

    Add score schema to model

--- data/lecture.js
+++ data/lecture.js
@@ -1,13 +1,21 @@
 var mongoose = require('mongoose');
+var timestamps = require('mongoose-timestamp');
 
 var Schema = mongoose.Schema,
   ObjectId = Schema.ObjectId;
+
+var scoreSchema = new Schema({
+  total: Number,
+  confused: Number,
+});
+scoreSchema.plugin(timestamps);
 
 var lectureSchema = new Schema({
   slug: String,
   className: String,
   lecturerName: String,
-  topic: String
+  topic: String,
+  scores: [scoreSchema]
 });
 
 var Lecture = mongoose.model('Lecture', lectureSchema);
